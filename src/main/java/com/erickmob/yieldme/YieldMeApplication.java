6d3b6a718262b7646f368dcb32569c36104070c5
erickmob <erickmob@gmail.com>
2021-01-05 09:55:03 -0300

    first

---
+++ src/main/java/com/erickmob/yieldme/YieldMeApplication.java
@@ -0,0 +1,13 @@
+package com.erickmob.yieldme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YieldMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YieldMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erickmob/yieldme/YieldMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickmob.yieldme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YieldMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
