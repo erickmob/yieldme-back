b27a6da7200aa128833dd7ac08ce0ded44834183
erickmob <erickmob@gmail.com>
2021-01-05 11:21:47 -0300

    security and profiles

--- src/main/java/com/erickmob/yieldme/YieldMeApplication.java
+++ src/main/java/com/erickmob/yieldme/YieldMeApplication.java
@@ -1,13 +1,48 @@
 package com.erickmob.yieldme;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import com.erickmob.yieldme.model.Role;
+import com.erickmob.yieldme.model.User;
+import com.erickmob.yieldme.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class YieldMeApplication {
+public class YieldMeApplication implements CommandLineRunner {
+
+	@Autowired
+	UserService userService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(YieldMeApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+	@Override
+	public void run(String... params) throws Exception {
+		User admin = new User();
+		admin.setUsername("admin");
+		admin.setPassword("admin");
+		admin.setEmail("admin@yieldme.com");
+		admin.setRoles(new ArrayList<Role>(Arrays.asList(Role.ROLE_ADMIN)));
+
+		userService.signup(admin);
+
+		User client = new User();
+		client.setUsername("client");
+		client.setPassword("client");
+		client.setEmail("client@yieldme.com");
+		client.setRoles(new ArrayList<Role>(Arrays.asList(Role.ROLE_CLIENT)));
+
+		userService.signup(client);
+	}
 }
